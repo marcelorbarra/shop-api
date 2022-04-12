55b6fd8bc13d39c30774adeba9d754046684adfd
Marcelo Barra <mbarra14@gmail.com>
2022-04-11 22:52:53 -0300

    commit inicial

---
+++ src/main/java/br/com/marcelorbarra/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package br.com.marcelorbarra.repository;
+
+import br.com.marcelorbarra.model.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}
